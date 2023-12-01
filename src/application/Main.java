d0fff521f4f539a3eb6a1ba6ae59e087639f14b6
Vijay Singh <vijaysinghaashu@gmail.com>
2023-12-01 13:53:29 +0530

    javafx demo

---
+++ src/application/Main.java
@@ -0,0 +1,10 @@
+package application;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+}
